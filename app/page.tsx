acc08924cd9ce47a497f5fcf0736367921cacb29
ShadowAdi <shadowshukla76@gmail.com>
2026-03-15 01:17:04 +0530

    remove unnessary code

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main className="flex flex-col min-w-screen min-h-screen py-3"></main>
+    <main className="flex flex-col min-w-screen min-h-screen py-3"></main>
   );
 }
